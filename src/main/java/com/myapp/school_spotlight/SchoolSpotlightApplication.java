4e6508d154cb92116574b0f18e6909d7596185fb
swaminan <swaminan@purdue.edu>
2024-12-23 00:22:27 -0500

    project created with springboot

---
+++ src/main/java/com/myapp/school_spotlight/SchoolSpotlightApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.school_spotlight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSpotlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolSpotlightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/school_spotlight/SchoolSpotlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.school_spotlight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSpotlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
